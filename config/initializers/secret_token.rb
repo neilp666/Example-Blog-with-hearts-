54b8632f2ed357878d0225729999903f3eeb1f3b
neil patel <neil@myindianweddingapp.com>
2014-03-23 10:50:35 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crumblr::Application.config.secret_key_base = 'f43f583f0da2326bfec8a31ebc35fe5c102f5160f098a7eb6a9205f27e32be10426dcf06304aa47f950dbeb0efa460d6a28681cbe8a6192b413d64e05974e0fe'
